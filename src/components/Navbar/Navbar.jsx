df2de085d98a3bf3dd8fe4ed141d91f7b9ebf188
ihugo1 <huglainez@gmail.com>
2025-08-02 14:30:47 -0600

    Add products ammount on cart icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,34 +6,47 @@ import { Cart } from "../Cart/Cart";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
+import { useCart } from "../../context/cartContext";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const { cartProducts } = useCart();
   const location = useLocation();
 
   const handleToggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const handleToggleCart = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className={`
-    ${styles.navbar} ${(location.pathname!=="/" || isMenuOpen) ? styles.solid : styles.transparent}
-    `}>
+    <div
+      className={`
+    ${styles.navbar} ${
+        location.pathname !== "/" || isMenuOpen
+          ? styles.solid
+          : styles.transparent
+      }
+    `}
+    >
       <Link className={styles.logo} to="/">
         <p className={styles.a}>PRETTY</p>
         <p className={styles.b}>PINK</p>
       </Link>
 
       <div className={styles.navButtons}>
-        <button onClick={()=>setIsCartOpen(true)}> 
+        <button onClick={() => setIsCartOpen(true)}>
           <FaShoppingCart />
+          {cartProducts.length > 0 ? (
+            <p className={styles.totalProducts}>{cartProducts.length}</p>
+          ) : (
+            <div></div>
+          )}
         </button>
         <button onClick={handleToggleMenu}>
           {isMenuOpen ? <FaX /> : <FaBars />}
         </button>
       </div>
 
-      <Cart isOpen={isCartOpen} closeCart={handleToggleCart}/>
+      <Cart isOpen={isCartOpen} closeCart={handleToggleCart} />
       <Menu isOpen={isMenuOpen} />
     </div>
   );
